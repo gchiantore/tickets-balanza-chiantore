6bdb8685eb4d8bc14bc973b2adecc91028e3f0eb
gchiantore <102333574+gchiantore@users.noreply.github.com>
2024-07-30 16:26:16 -0300

    .

--- sql_project/ejemplos/vistas.sql
+++ sql_project/ejemplos/vistas.sql
@@ -1,6 +1,6 @@
 -- Ejemplo de la vista TICKETS POR TURNOS, 
 
-select * from TICKETS_POR_TURNOS where FECHA='2024-01-15' and IDTURNO=1;
+select * from TICKETS_POR_TURNOS where FECHA='2024-01-15';
 
 -- Ejemplo de la vista Cantidad de Tickets por turno y fecha
 
