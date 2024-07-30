00fabb6be60dbfe00ddc72f283a84686317748a6
gchiantore <102333574+gchiantore@users.noreply.github.com>
2024-07-30 16:04:22 -0300

    .

---
+++ sql_project/ejemplos/stored_procedures.sql
@@ -0,0 +1,7 @@
+-- Agregar un Operario
+
+CALL CREA_OPERARIO('GONZALEZ, JUAN CRUZ','974-604-9127','jcgonzales@pepito.com',1);
+
+-- Agregar un producto 
+
+CALL CREA_PRODUCTO('OTROS',5,'/img/iconos/maiz.png');
--- sql_project/ejemplos/vistas.sql
+++ sql_project/ejemplos/vistas.sql
@@ -4,4 +4,8 @@ select * from TICKETS_POR_TURNOS where FECHA='2024-01-15' and IDTURNO=1;
 
 -- Ejemplo de la vista Cantidad de Tickets por turno y fecha
 
-select * from CANTIDAD_TICKETS_POR_TURNOS where fecha='2024-01-15'
+select * from CANTIDAD_TICKETS_POR_TURNOS where fecha='2024-01-15';
+
+-- Ejemplo Tipo de productos - Productos 
+
+select * from TIPODEPRODUCTOS_PRODUCTOS;
